8598ad1c1ffc9792a622d29698a929ca806b1f58
Hyangim Kim <73486274+Mimi8O@users.noreply.github.com>
2024-08-06 20:17:11 +0900

    Update detection_demo.py

--- detection_demo.py
+++ detection_demo.py
@@ -17,11 +17,18 @@ from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_
 from yolov3.configs import *
 
 image_path   = "./IMAGES/kite.jpg"
+output_path  = "/kaggle/working/kite_pred.jpg"  # 결과 이미지를 저장할 경로
 video_path   = "./IMAGES/test.mp4"
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+detect_image(yolo, image_path, output_path, input_size=YOLO_INPUT_SIZE, rectangle_colors=(255,0,0))
 #detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
 #detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
 
 #detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0), realtime=False)
+
+# 이미지 저장 확인
+if os.path.exists(output_path):
+    print(f"이미지가 성공적으로 저장되었습니다: {output_path}")
+else:
+    print("이미지가 저장되지 않았습니다.")
