e229b9912cee0820015c3c01718e3fee12ff4c80
Hyangim Kim <73486274+Mimi8O@users.noreply.github.com>
2024-08-06 21:57:48 +0900

    Update detect_mnist.py

--- detect_mnist.py
+++ detect_mnist.py
@@ -13,27 +13,26 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import cv2
 import numpy as np
 import random
-import time
 import tensorflow as tf
 from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import detect_image
 from yolov3.configs import *
 
-while True:
-    ID = random.randint(0, 200)
-    label_txt = "mnist/mnist_test.txt"
-    image_info = open(label_txt).readlines()[ID].split()
+# 무작위로 하나의 이미지를 선택
+ID = random.randint(0, 200)
+label_txt = "mnist/mnist_test.txt"
+image_info = open(label_txt).readlines()[ID].split()
 
-    image_path = image_info[0]
-    output_path = "/kaggle/working/mnist_test.jpg"  # 결과 이미지를 저장할 경로
+image_path = image_info[0]
+output_path = "/kaggle/working/mnist_test.jpg"  # 결과 이미지를 저장할 경로
 
-    yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
-    yolo.load_weights(f"./checkpoints/{TRAIN_MODEL_NAME}")  # use keras weights
+yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
+yolo.load_weights(f"./checkpoints/{TRAIN_MODEL_NAME}")  # use keras weights
 
-    detect_image(yolo, image_path, output_path, input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+detect_image(yolo, image_path, output_path, input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 
-    # 이미지 저장 확인
-    if os.path.exists(output_path):
-        print(f"이미지가 성공적으로 저장되었습니다: {output_path}")
-    else:
-        print("이미지가 저장되지 않았습니다.")
+# 이미지 저장 확인
+if os.path.exists(output_path):
+    print(f"이미지가 성공적으로 저장되었습니다: {output_path}")
+else:
+    print("이미지가 저장되지 않았습니다.")
